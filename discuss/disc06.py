e688d8e838c9728eef80f0968355fde498bcebc4
fobiddengame <panxiezhao@gmail.com>
2022-06-09 00:14:40 +0800

    disc06_Q4&Q5

--- discuss/disc06.py
+++ discuss/disc06.py
@@ -58,3 +58,41 @@ def max_path_sum(t):
     else:
         return t.label + max(max_path_sum(b) for b in t.branches)
 
+
+def find_path(t, x):
+    """
+    >>> t = Tree(2, [Tree(7, [Tree(3), Tree(6, [Tree(5), Tree(11)])]), Tree(15)])
+    >>> find_path(t, 5)
+    [2, 7, 6, 5]
+    >>> find_path(t, 10)  # returns None
+    """
+    if t.label == x:
+        return [t.label]
+    for b in t.branches:
+        path = find_path(b, x)
+        if path:
+            return [t.label] + path
+
+
+def prune_small(t, n):
+    """Prune the tree mutatively, keeping only the n branches
+    of each node with the smallest label.
+
+    >>> t1 = Tree(6)
+    >>> prune_small(t1, 2)
+    >>> t1
+    Tree(6)
+    >>> t2 = Tree(6, [Tree(3), Tree(4)])
+    >>> prune_small(t2, 1)
+    >>> t2
+    Tree(6, [Tree(3)])
+    >>> t3 = Tree(6, [Tree(1), Tree(3, [Tree(1), Tree(2), Tree(3)]), Tree(5, [Tree(3), Tree(4)])])
+    >>> prune_small(t3, 2)
+    >>> t3
+    Tree(6, [Tree(1), Tree(3, [Tree(1), Tree(2)])])
+    """
+    while len(t.branches) > n:
+        largest = max(t.branches, key=lambda x: x.label)
+        t.branches.remove(largest)
+    for b in t.branches:
+        prune_small(b, n)
