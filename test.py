10e74e75e65cfc88ed0b81cae56d817c43087f7d
fobiddengame <panxiezhao@gmail.com>
2022-06-10 01:47:55 +0800

    hw05_chains

--- test.py
+++ test.py
@@ -1,81 +1,45 @@
-class Tree:
+class Link:
+    """A linked list."""
+    empty = ()
 
-    def __init__(self, label, branches=[]):
-        self.label = label
-        for branch in branches:
-            assert isinstance(branch, Tree)
-        self.branches = list(branches)
-
-    def is_leaf(self):
-        return not self.branches
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
 
     def __repr__(self):
-        if self.branches:
-            branch_str = ', ' + repr(self.branches)
+        if self.rest:
+            rest_repr = ', ' + repr(self.rest)
         else:
-            branch_str = ''
-        return 'Tree({0}{1})'.format(self.label, branch_str)
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
 
     def __str__(self):
-        return '\n'.join(self.indented())
-
-    def indented(self):
-        lines = []
-        for b in self.branches:
-            for line in b.indented():
-                lines.append('  ' + line)
-        return [str(self.label)] + lines
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
 
 
-def leaves(t):
-    """Return a list containing the leaf labels of T.
-    >>> t = Tree(20, [Tree(12, [Tree(9, [Tree(7), Tree(2)]), Tree(3)]), Tree(8, [Tree(4), Tree(4)])])
-    >>> leaves(t)
-    [7, 2, 3, 4, 4]
+def add(ordered_list, new_val):
+    """Add NEW_VAL to ORDERED_LIST, returning modified ORDERED_LIST.
+    >>> s = Link(1, Link(3, Link(5)))
+    >>> add(s, 0)
+    Link(0, Link(1, Link(3, Link(5))))
+    >>> add(s, 3)
+    Link(0, Link(1, Link(3, Link(5))))
+    >>> add(s, 4)
+    Link(0, Link(1, Link(3, Link(4, Link(5)))))
+    >>> add(s, 6)
+    Link(0, Link(1, Link(3, Link(4, Link(5, Link(6))))))
     """
-    if t.is_leaf():
-        return [t.label]
-    else:
-        return sum([leaves(b) for b in t.branches], [])
-
-
-def count_paths(t, total):
-    """Return the number of paths from the root to any node in T
-    for which the labels along the path sum to TOTAL.
-
-    >>> t = Tree(3, [Tree(-1), Tree(1, [Tree(2, [Tree(1)]), Tree(3)]), Tree(1, [Tree(-1)])])
-    >>> count_paths(t, 3)
-    2
-    >>> count_paths(t, 4)
-    2
-    >>> count_paths(t, 5)
-    0
-    >>> count_paths(t, 6)
-    1
-    >>> count_paths(t, 7)
-    2
-    """
-    if t.label == total:
-        found = 1
-    else:
-        found = 0
-
-    return found + sum([count_paths(b, total - t.label) for b in t.branches])
-
-
-def height(t):
-    """Return the height of a tree.
-
-    >>> t = Tree(3, [Tree(5, [Tree(1)]), Tree(2)])
-    >>> height(t)
-    2
-    >>> t = Tree(3, [Tree(1), Tree(2, [Tree(5, [Tree(6)]), Tree(1)])])
-    >>> height(t)
-    3
-    """
-    "*** YOUR CODE HERE ***"
-
-    if t.is_leaf():
-        return 0
-    else:
-        return 1 + max([height(b) for b in t.branches])
+    if new_val < ordered_list.first:
+        original_first = ordered_list.first
+        ordered_list.first = new_val
+        ordered_list.rest = Link(original_first, ordered_list.rest)
+    elif new_val > ordered_list.first and ordered_list.rest is Link.empty:
+        ordered_list.rest = Link(new_val)
+    elif new_val > ordered_list.first:
+        add(ordered_list.rest, new_val)
+    return ordered_list
