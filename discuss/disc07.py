64a004312c1bcdb570812ab7d425a80a82df280f
fobiddengame <panxiezhao@gmail.com>
2022-06-12 23:01:47 +0800

    add another recursive method

--- discuss/disc07.py
+++ discuss/disc07.py
@@ -61,3 +61,31 @@ def remove_all(link, value):
         else:
             p1 = p2
         p2 = p2.rest
+
+
+def remove_all(link, value):
+    """Remove all the nodes containing value in link. Assume that the
+    first element is never removed.
+
+    Another recursive method.
+    >>> l1 = Link(0, Link(2, Link(2, Link(3, Link(1, Link(2, Link(3)))))))
+    >>> print(l1)
+    <0 2 2 3 1 2 3>
+    >>> remove_all(l1, 2)
+    >>> print(l1)
+    <0 3 1 3>
+    >>> remove_all(l1, 3)
+    >>> print(l1)
+    <0 1>
+    >>> remove_all(l1, 3)
+    >>> print(l1)
+    <0 1>
+    """
+    "*** YOUR CODE HERE ***"
+    if link is Link.empty or link.rest is Link.empty:
+        return
+    if link.rest.first == value:
+        link.rest = link.rest.rest
+        remove_all(link, value)
+    else:
+        remove_all(link.rest, value)
