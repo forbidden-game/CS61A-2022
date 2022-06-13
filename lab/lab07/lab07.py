f0b2aaba8c2dac9ef6cb4e0fc454f0b2e9664512
fobiddengame <panxiezhao@gmail.com>
2022-06-13 21:28:25 +0800

    lab07 cumulative_mul

--- lab/lab07/lab07.py
+++ lab/lab07/lab07.py
@@ -43,6 +43,11 @@ def cumulative_mul(t):
     Tree(5040, [Tree(60, [Tree(3), Tree(4), Tree(5)]), Tree(42, [Tree(7)])])
     """
     "*** YOUR CODE HERE ***"
+    if not t.is_leaf():
+        for b in t.branches:
+            cumulative_mul(b)
+            t.label *= b.label
+
 
 
 def has_cycle(link):
