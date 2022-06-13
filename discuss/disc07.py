7891e940710c6bb0dc71775d94cf3c17bd39af75
fobiddengame <panxiezhao@gmail.com>
2022-06-13 14:58:28 +0800

    disc07 primes gen in decrease and ascending order

--- discuss/disc07.py
+++ discuss/disc07.py
@@ -130,3 +130,47 @@ def hailstone(n):
         yield from hailstone(n // 2)
     else:
         yield from hailstone(3 * n + 1)
+
+
+def is_prime(n):
+    """Returns True if n is a prime number and False otherwise.
+    >>> is_prime(2)
+    True
+    >>> is_prime(16)
+    False
+    >>> is_prime(521)
+    True
+    """
+    def helper(i):
+        if i > (n ** 0.5): # Could replace with i == n
+            return True
+        elif n % i == 0:
+            return False
+        return helper(i + 1)
+    return helper(2)
+
+
+def primes_gen(n):
+    """Generates primes in decreasing order.
+    >>> pg = primes_gen(7)
+    >>> list(pg)
+    [7, 5, 3, 2]
+    """
+    if n == 1:
+        return
+    if is_prime(n):
+        yield n
+    yield from primes_gen(n-1)
+
+
+def primes_gen(n, i):
+    """Generates primes in ascending order.
+    >>> pg = primes_gen(7, 2)
+    >>> list(pg)
+    [2, 3, 5, 7]
+    """
+    if i > n:
+        return
+    if is_prime(i):
+        yield i
+    yield from primes_gen(n, i+1)
