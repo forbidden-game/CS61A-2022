d0503bf99b47b37a5ebeec6efe513592f17f14f1
fobiddengame <panxiezhao@gmail.com>
2022-06-13 14:39:48 +0800

    disc07 hailstone

--- discuss/disc07.py
+++ discuss/disc07.py
@@ -89,3 +89,44 @@ def remove_all(link, value):
         remove_all(link, value)
     else:
         remove_all(link.rest, value)
+
+
+def filter_iter(iterable, f):
+    """
+    >>> is_even = lambda x: x % 2 == 0
+    >>> list(filter_iter(range(5), is_even)) # a list of the values yielded from the call to filter_iter
+    [0, 2, 4]
+    >>> all_odd = (2*y-1 for y in range(5))
+    >>> list(filter_iter(all_odd, is_even))
+    []
+    >>> naturals = (n for n in range(1, 100))
+    >>> s = filter_iter(naturals, is_even)
+    >>> next(s)
+    2
+    >>> next(s)
+    4
+    """
+    "*** YOUR CODE HERE ***"
+    for i in iterable:
+        if f(i):
+            yield i
+
+
+def hailstone(n):
+    """Yields the elements of the hailstone sequence starting at n.
+       At the end of the sequence, yield 1 infinitely.
+
+    >>> hail_gen = hailstone(10)
+    >>> [next(hail_gen) for _ in range(10)]
+    [10, 5, 16, 8, 4, 2, 1, 1, 1, 1]
+    >>> next(hail_gen)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    yield n
+    if n == 1:
+        yield from hailstone(1)
+    elif n % 2 == 0:
+        yield from hailstone(n // 2)
+    else:
+        yield from hailstone(3 * n + 1)
