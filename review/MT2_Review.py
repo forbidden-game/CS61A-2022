ea03be522ecefc8576f4131145b045de313d2d36
fobiddengame <panxiezhao@gmail.com>
2022-06-14 00:34:22 +0800

    MT2_Review

---
+++ review/MT2_Review.py
@@ -0,0 +1,86 @@
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.first = 5
+    >>> s.rest.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+
+def merge(s, t):
+    """Return a sorted Link containing the elements of sorted s & t.
+
+    >>> a = Link(1, Link(5))
+    >>> b = Link(1, Link(4))
+    >>> merge(a, b)
+    Link(1, Link(1, Link(4, Link(5))))
+    >>> a
+    Link(1, Link(5))
+    >>> b
+    Link(1, Link(4))
+    """
+    if s is Link.empty:
+        return t
+    elif t is Link.empty:
+        return s
+    elif s.first <= t.first:
+        return Link(s.first, merge(s.rest, t))
+    else:
+        return Link(t.first, merge(s, t.rest))
+
+
+def merge_in_place(s, t):
+    """Return a sorted Link containing the elements of sorted s & t.
+
+    >>> a = Link(1, Link(5))
+    >>> b = Link(1, Link(4))
+    >>> merge_in_place(a, b)
+    Link(1, Link(1, Link(4, Link(5))))
+    >>> a
+    Link(1, Link(1, Link(4, Link(5))))
+    >>> b
+    Link(1, Link(4, Link(5)))
+    """
+    if s is Link.empty:
+        return t
+    elif t is Link.empty:
+        return s
+    elif s.first <= t.first:
+        s.rest = merge_in_place(s.rest, t)
+        return s
+    else:
+        t.rest = merge_in_place(s, t.rest)
+        return t
