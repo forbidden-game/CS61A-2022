81b0b21a90d6ec6a239ab86ec396a76946094749
fobiddengame <panxiezhao@gmail.com>
2022-06-15 23:59:02 +0800

    subseqs

--- lab/lab08/lab08.py
+++ lab/lab08/lab08.py
@@ -170,9 +170,6 @@ def add_d_leaves(t, v):
     return add_d_leaves_helper(t, 0)
 
 
-
-
-
 def insert_into_all(item, nested_list):
     """Return a new list consisting of all the lists in nested_list,
     but with item added to the front of each. You can assume that
@@ -181,25 +178,36 @@ def insert_into_all(item, nested_list):
     >>> nl = [[], [1, 2], [3]]
     >>> insert_into_all(0, nl)
     [[0], [0, 1, 2], [0, 3]]
+    >>> nl = [[]]
+    >>> insert_into_all(0, nl)
+    [[0]]
     """
     "*** YOUR CODE HERE ***"
+    for i in nested_list:
+        i[0:0] = [item]
+    return nested_list
 
 
 def subseqs(s):
     """Return a nested list (a list of lists) of all subsequences of S.
     The subsequences can appear in any order. You can assume S is a list.
 
+    >>> seqs = subseqs([1, 2])
+    >>> sorted(seqs)
+    [[], [1], [1, 2], [2]]
     >>> seqs = subseqs([1, 2, 3])
     >>> sorted(seqs)
     [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
+    >>> subseqs([1])
+    [[], [1]]
     >>> subseqs([])
     [[]]
     """
-    if ________________:
-        ________________
+    if not s:
+        return [[]]
     else:
-        ________________
-        ________________
+        sub = subseqs(s[1:])[:]
+        return sub + insert_into_all(s[0], subseqs(s[1:]))
 
 
 def non_decrease_subseqs(s):
