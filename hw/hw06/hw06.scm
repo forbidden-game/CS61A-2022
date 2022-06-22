93e8717fe6fce87d53aa2e85a3f7baff27e6a290
fobiddengame <panxiezhao@gmail.com>
2022-06-23 01:00:57 +0800

    hw06

--- hw/hw06/hw06.scm
+++ hw/hw06/hw06.scm
@@ -12,8 +12,23 @@
     ((< (car lst) (cadr lst)) (ascending? (cdr lst)))
     (else #f)))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave list1 list2)
+    (if (or (null? list1) (null? list2))
+        (append list1 list2)
+        (cons (car list1)
+              (cons (car list2)
+                    (interleave (cdr list1) (cdr list2))))))
 
-(define (my-filter func lst) 'YOUR-CODE-HERE)
 
-(define (no-repeats lst) 'YOUR-CODE-HERE)
+(define (my-filter func lst)
+  (if (null? lst)
+    nil
+    (cond ((func (car lst)) (cons (car lst) (my-filter func (cdr lst))))
+        (else (my-filter func (cdr lst))))))
+
+(define (no-repeats s)
+  (if (null? s)
+    nil
+    (cons
+      (car s)
+      (no-repeats (my-filter (lambda (x) (not (eq? x (car s)))) (cdr s))))))
