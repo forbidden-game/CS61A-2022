7e8fdfc5bde14ec6e1d9ab164fd81493069d48b6
fobiddengame <panxiezhao@gmail.com>
2022-06-22 16:28:51 +0800

    hw06 Q1

---
+++ hw/hw06/hw06.scm
@@ -0,0 +1,15 @@
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s)
+  (car (cdr s)))
+
+(define (caddr s)
+  (car (cdr (cdr s))))
+
+(define (ascending? lst) 'YOUR-CODE-HERE)
+
+(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+
+(define (my-filter func lst) 'YOUR-CODE-HERE)
+
+(define (no-repeats lst) 'YOUR-CODE-HERE)
