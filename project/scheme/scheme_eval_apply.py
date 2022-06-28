256694aefc56521fa2a167397e004e982b20b0db
fobiddengame <panxiezhao@gmail.com>
2022-06-29 02:05:18 +0800

    p9

--- project/scheme/scheme_eval_apply.py
+++ project/scheme/scheme_eval_apply.py
@@ -64,6 +64,8 @@ def scheme_apply(procedure, args, env):
     elif isinstance(procedure, LambdaProcedure):
         # BEGIN PROBLEM 9
         "*** YOUR CODE HERE ***"
+        new_frame = procedure.env.make_child_frame(procedure.formals, args)
+        return eval_all(procedure.body, new_frame)
         # END PROBLEM 9
     elif isinstance(procedure, MuProcedure):
         # BEGIN PROBLEM 11
