84bea4c735d7bad211ce7089d48f1a44b983c084
fobiddengame <panxiezhao@gmail.com>
2022-07-02 17:02:03 +0800

    p16

--- project/scheme/questions.scm
+++ project/scheme/questions.scm
@@ -1,83 +1,90 @@
 (define (caar x) (car (car x)))
+
 (define (cadr x) (car (cdr x)))
+
 (define (cdar x) (cdr (car x)))
+
 (define (cddr x) (cdr (cdr x)))
 
-;; Problem 15
-;; Returns a list of two-element lists
+; ; Problem 15
+; ; Returns a list of two-element lists
 (define (enumerate s)
   ; BEGIN PROBLEM 15
   (define (enumerate2 s num)
-      (if (null? s)
-          nil
-          (cons (list num (car s)) (enumerate2 (cdr s) (+ num 1)))
-      ))
-  (enumerate2 s 0)
-  )
-  ; END PROBLEM 15
+    (if (null? s)
+        nil
+        (cons (list num (car s))
+              (enumerate2 (cdr s) (+ num 1)))))
+  (enumerate2 s 0))
 
-;; Problem 16
-
-;; Merge two lists LIST1 and LIST2 according to INORDER? and return
-;; the merged lists.
-(define (merge inorder? list1 list2)
+; END PROBLEM 15
+; ; Problem 16
+; ; Merge two lists LIST1 and LIST2 according to INORDER? and return
+; ; the merged lists.
+(define (merge comp list1 list2)
   ; BEGIN PROBLEM 16
-  'replace-this-line
-  )
-  ; END PROBLEM 16
+  (cond 
+    ((null? list1)
+     list2)
+    ((null? list2)
+     list1)
+    ((comp (car list1) (car list2))
+     (cons (car list1) (merge comp (cdr list1) list2)))
+    (else
+     (cons (car list2) (merge comp list1 (cdr list2))))))
 
-
-;; Optional Problem 1
-
-;; Returns a function that checks if an expression is the special form FORM
+; END PROBLEM 16
+; ; Optional Problem 1
+; ; Returns a function that checks if an expression is the special form FORM
 (define (check-special form)
   (lambda (expr) (equal? form (car expr))))
 
 (define lambda? (check-special 'lambda))
+
 (define define? (check-special 'define))
+
 (define quoted? (check-special 'quote))
-(define let?    (check-special 'let))
 
-;; Converts all let special forms in EXPR into equivalent forms using lambda
+(define let? (check-special 'let))
+
+; ; Converts all let special forms in EXPR into equivalent forms using lambda
 (define (let-to-lambda expr)
-  (cond ((atom? expr)
-         ; BEGIN PROBLEM 17
-         'replace-this-line
-         ; END PROBLEM 17
-         )
-        ((quoted? expr)
-         ; BEGIN PROBLEM 17
-         'replace-this-line
-         ; END PROBLEM 17
-         )
-        ((or (lambda? expr)
-             (define? expr))
-         (let ((form   (car expr))
-               (params (cadr expr))
-               (body   (cddr expr)))
-           ; BEGIN PROBLEM 17
-           'replace-this-line
-           ; END PROBLEM 17
-           ))
-        ((let? expr)
-         (let ((values (cadr expr))
-               (body   (cddr expr)))
-           ; BEGIN PROBLEM 17
-           'replace-this-line
-           ; END PROBLEM 17
-           ))
-        (else
-         ; BEGIN PROBLEM 17
-         'replace-this-line
-         ; END PROBLEM 17
-         )))
+  (cond 
+    ((atom? expr)
+     ; BEGIN PROBLEM 17
+     'replace-this-line
+     ; END PROBLEM 17
+    )
+    ((quoted? expr)
+     ; BEGIN PROBLEM 17
+     'replace-this-line
+     ; END PROBLEM 17
+    )
+    ((or (lambda? expr) (define? expr))
+     (let ((form (car expr))
+           (params (cadr expr))
+           (body (cddr expr)))
+       ; BEGIN PROBLEM 17
+       'replace-this-line
+       ; END PROBLEM 17
+     ))
+    ((let? expr)
+     (let ((values (cadr expr))
+           (body (cddr expr)))
+       ; BEGIN PROBLEM 17
+       'replace-this-line
+       ; END PROBLEM 17
+     ))
+    (else
+     ; BEGIN PROBLEM 17
+     'replace-this-line
+     ; END PROBLEM 17
+    )))
 
-
-
-;; Problem 21 (optional)
-;; Draw the hax image using turtle graphics.
+; ; Problem 21 (optional)
+; ; Draw the hax image using turtle graphics.
 (define (hax d k)
   ; BEGIN Question 21
-  'replace-this-line
-  )
-  ; END Question 21
+  'replace-this-line)
+
+; END Question 21
